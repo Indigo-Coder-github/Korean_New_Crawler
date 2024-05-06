f5ea87f2abd1ca738f172b2ed2b9fd3b20072e16
Indigo-Coder <hjs40111@gmail.com>
2024-05-06 16:55:48 +0900

    modify long description with version modification

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="korean_news_crawler",
-    version="1.0.0",
+    version="1.0.1",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Python Library for Crawling Top 10 Korean News and Providing Synonym Dictionary",
     author="Indigo-Coder",
     author_email="hjs40111@gmail.com",
-    description="Python Library for Crawling Top 10 Korean News and Providing Synonym Dictionary",
-    long_description_content_type="text/markdown",
     url="https://github.com/Indigo-Coder-github/Korean_News_Crawler",
     install_requires=[
         "BeautifulSoup4",
